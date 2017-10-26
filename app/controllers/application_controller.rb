fe2ea16a9c6bc61e0b56b62ad3827dae43115dca
Mary Benitez <meebenitez@gmail.com>
2017-10-26 00:39:32 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,68 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/tweets/new' do
+    erb :'tweets/create_tweet'
+  end
+
+  post '/tweets' do
+    erb :'tweets/tweets'
+
+  end
+
+  get '/tweets/:id' do
+    erb :'tweets/show_tweet'
+  end
+
+  get '/tweets/:id/edit' do
+    erb :'tweets/edit_tweet'
+  end
+
+  post '/tweets/:id' do
+
+  end
+
+  post '/tweets/:id/delete' do
+
+  end
+
+  get '/signup' do
+    erb :'users/create_user'
+
+  end
+
+  post '/signup' do
+
+  end
+
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+
+  end
+
+  get 'logout'
+    session.clear
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
