a5806cc826760765c3cd6641644aa4ebd317cf0f
Mary Benitez <meebenitez@gmail.com>
2017-10-27 02:44:25 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,58 +5,58 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    enable :sessions
-    set :session_secret, "password_security"
+    #enable :sessions
+    #set :session_secret, "password_security"
   end
 
   get '/' do
     erb :index
   end
-
-  get '/tweets/new' do
-    erb :'tweets/create_tweet'
-  end
-
-  post '/tweets' do
-    erb :'tweets/tweets'
-  end
-
-  get '/tweets/:id' do
-    erb :'tweets/show_tweet'
-  end
-
-  get '/tweets/:id/edit' do
-    erb :'tweets/edit_tweet'
-  end
-
-  post '/tweets/:id' do
-
-  end
-
-  post '/tweets/:id/delete' do
-
-  end
-
-  get '/signup' do
-    erb :'users/create_user'
-
-  end
-
-  post '/signup' do
-
-  end
-
-  get '/login' do
-    erb :'users/login'
-  end
-
-  post '/login' do
-
-  end
-
-  get 'logout' do
-    session.clear
-  end
+  #
+  # get '/tweets/new' do
+  #   erb :'tweets/create_tweet'
+  # end
+  #
+  # post '/tweets' do
+  #   erb :'tweets/tweets'
+  # end
+  #
+  # get '/tweets/:id' do
+  #   erb :'tweets/show_tweet'
+  # end
+  #
+  # get '/tweets/:id/edit' do
+  #   erb :'tweets/edit_tweet'
+  # end
+  #
+  # post '/tweets/:id' do
+  #
+  # end
+  #
+  # post '/tweets/:id/delete' do
+  #
+  # end
+  #
+  # get '/signup' do
+  #   erb :'users/create_user'
+  #
+  # end
+  #
+  # post '/signup' do
+  #
+  # end
+  #
+  # get '/login' do
+  #   erb :'users/login'
+  # end
+  #
+  # post '/login' do
+  #
+  # end
+  #
+  # get 'logout' do
+  #   session.clear
+  # end
 
   helpers do
     def logged_in?
